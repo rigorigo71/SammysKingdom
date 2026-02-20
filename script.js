949573e5ac142c4294b8a5ec70d573962c912111
rigorigo71 <rigolemus2001@gmail.com>
2026-02-19 20:14:53 -0800

    Added to all files. Star trail finished.

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 const root = document.getElementById("app");
 
-const urls = ["/svg/1.svg", "/svg/2.svg", "/svg/3.svg", "./svg/4.svg", "/svg/5.svg"];
+const urls = ["/svg/1.svg", "/svg/2.svg", "/svg/3.svg", "/svg/4.svg", "/svg/5.svg"];
 const classes = ["star-1", "star-2", "star-3", "star-4", "star-5"];
-
-const star = document.createElement("img")
-const idx = Math.floor(Math.random() * classes.length);
-star.classList.add("star", classes[idx]);
-star.setAttribute(
-    "style"
-    `
+function createStar(x, y) {
+    const star = document.createElement("img")
+    const idx = Math.floor(Math.random() * classes.length);
+    star.classList.add("star", classes[idx]);
+    // star.classList.add(classes[Math.floor(Math.random()*classes.length)])
+    star.setAttribute("src", urls[idx]);
+    star.setAttribute(
+        "style",
+        `
     position:fixed;
     top: ${y}px;
     left: ${x}px;
 `
-)
-root.appendChild(star);
-setTimeout(()=>star.remove())
+    )
+    root.appendChild(star);
+    setTimeout(() => star.remove(), 500);
+    return star;
+}
+window.addEventListener("mousemove", (e) => {
+    requestAnimationFrame(function () {
+        createStar(e.clientX, e.clientY);
+    })
+})
